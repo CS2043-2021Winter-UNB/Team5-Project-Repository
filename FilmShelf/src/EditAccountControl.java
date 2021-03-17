754b849dfa1a82ac03017066bd5adec15dfa99d5
salamiman <josulaiman@hotmail.com>
2021-03-17 00:07:20 -0300

    Made progress on EditAccountControl.
    - Recycled validateFormInput() from CreateMemberAccountControl.
    - Created draft of updateAccount(). Have some questions to bring forward with the team that will determine what this will end up looking like.
    - Removed returnEditAccountConfirmation().

--- FilmShelf/src/EditAccountControl.java
+++ FilmShelf/src/EditAccountControl.java
@@ -1,27 +1,168 @@
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/******************************************************************************************************************************
+ * EditAccountControl
+ * @author Jo
+ * Description: 
+ ******************************************************************************************************************************/
 public class EditAccountControl {
 
-	private Class members;
 	private DataManager dataManager;
-
-	public void validateFormInput() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	private LoginControl loginControl;
+	private Pattern uppercasePattern;
+	private Pattern lowercasePattern;
+	private Pattern numberPattern;
+	private Pattern specialCharacterPattern;
+	private Matcher uppercaseMatcher;
+	private Matcher lowercaseMatcher;
+	private Matcher numberMatcher;
+	private Matcher specialCharacterMatcher;
+	
+	// class constructor
+	public EditAccountControl(DataManager dataManager, LoginControl loginControl) {
+		this.dataManager = dataManager;
+		this.loginControl = loginControl;
+		this.uppercasePattern = Pattern.compile("[A-Z]");					// matches uppercase letters
+		this.lowercasePattern = Pattern.compile("[a-z]");					// matches lowercase letters
+		this.numberPattern = Pattern.compile("[0-9]");						// matches numbers
+		this.specialCharacterPattern = Pattern.compile("[^A-Za-z0-9]");		// matches special characters
 	}
 
-	public void updateAccount() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	// may need two validation methods with different signatures
+	// one to check member details, one to check admin details
+	public boolean validateFormInput(String username, String password, String firstName, String lastName) {
+		boolean inputIsValid = false;
+		
+		// only verify input if non-null values are provided
+		if((username != null) && (password != null) && (firstName != null) && (lastName != null)) {
+			// remove leading and trailing whitespace and store in new variable
+			String uName = username.strip();
+			String pWord = password.strip();
+			String fName = firstName.strip();
+			String lName = lastName.strip();
+		
+			// check validity of each parameter
+			// check username
+				// limit of 25 characters
+				// no special symbols
+			specialCharacterMatcher = specialCharacterPattern.matcher(uName);
+		
+			if(specialCharacterMatcher.find() || (uName.length() > 25) || (uName.length() < 6)) {
+				inputIsValid = false;
+			} else {
+				inputIsValid = true;	// only set true here. After, only set false to avoid false positives
+			}
+		
+			// check password
+				// at least 8 characters long
+				// contain uppercase letter, lowercase letter, number, special character
+			if(inputIsValid) {
+				uppercaseMatcher = uppercasePattern.matcher(pWord);
+				lowercaseMatcher = lowercasePattern.matcher(pWord);
+				numberMatcher = numberPattern.matcher(pWord);
+				specialCharacterMatcher = specialCharacterPattern.matcher(pWord);
+			
+				if(!uppercaseMatcher.find() || !lowercaseMatcher.find() || !numberMatcher.find() || !specialCharacterMatcher.find()  || (pWord.length() < 8)) {
+					inputIsValid = false;
+				}
+			}
+		
+			// check first name
+				// limit of 25 characters
+				// no numbers or special characters
+			if(inputIsValid) {
+				numberMatcher = numberPattern.matcher(fName);
+				specialCharacterMatcher = specialCharacterPattern.matcher(fName);
+			
+				if(numberMatcher.find() || specialCharacterMatcher.find() || (fName.length() > 25) || (fName.length() < 1)) {
+					inputIsValid = false;
+				}
+			}
+		
+			// check last name
+				// limit of 25 characters
+				// no numbers or special characters
+			if(inputIsValid) {
+				numberMatcher = numberPattern.matcher(lName);
+				specialCharacterMatcher = specialCharacterPattern.matcher(lName);
+			
+				if(numberMatcher.find() || specialCharacterMatcher.find() || (lName.length() > 25) || (lName.length() < 1)) {
+					inputIsValid = false;
+				}
+			}
+		}
+		
+		return inputIsValid;	// return validity of input
 	}
 
-	public void returnEditAccountConfirmation() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	// Jo: 	how to determine if Member or Admin wants to edit account?
+	//		can we assume only one logged in user stored?
+	// 		ie. either currentAdmin or currentMember will be uninitialized
+	//		what details can users update?
+	//			Member: username, password, firstName, lastName
+	//			Admin: username, password
+	//		if username can be changed, will have to check database to ensure new username doesn't already exist
+	//		could be easier to assume only Members change details via app, Admin have details changed directly in database
+	public boolean updateAccount(String username, String password, String firstName, String lastName) {
+		boolean currentUserIsMember = false;
+		boolean inputIsValid = false;
+		boolean accountUpdated = false;
+		
+		// get current user logged in
+		MemberAccountObject member = loginControl.getCurrentMember();
+		AdminAccountObject admin = loginControl.getCurrentAdmin();
+		
+		// check if current user is member or admin
+		if((admin == null) && (member == null)) {
+			return false;
+		} else if(admin == null) {
+			currentUserIsMember = true;
+		} else if(member == null) {
+			currentUserIsMember = false;
+		}
+		
+		// only continue with edit account if at least one new input is provided
+		if(((username == null) || username.strip().isEmpty() || username.equals(member.getUsername())) &&
+			((password == null) || password.strip().isEmpty() || password.equals(member.getPassword())) &&
+			((firstName == null) || firstName.strip().isEmpty() || firstName.equals(member.getFirstName())) &&
+			((lastName == null) || lastName.strip().isEmpty() || lastName.equals(member.getLastName()))) {
+			return false;
+		}
+		
+		// check which details the member wants to change and which stay the same
+		// assume values not provided by user indicate fields that stay the same
+		if((username == null) || username.strip().isEmpty()) {
+			username = member.getUsername();
+		}
+		if((password == null) || password.strip().isEmpty()) {
+			password = member.getPassword();
+		}
+		if((firstName == null) || firstName.strip().isEmpty()) {
+			firstName = member.getFirstName();
+		}
+		if((lastName == null) || lastName.strip().isEmpty()) {
+			lastName = member.getLastName();
+		}
+		
+		// check validity of form input
+		if(currentUserIsMember) {
+			inputIsValid = validateFormInput(username, password, firstName, lastName);
+		} else if(!currentUserIsMember) {
+			inputIsValid = validateFormInput(username, password);
+		}
+		
+		// if input invalid, return false
+		if(!inputIsValid) {
+			accountUpdated = false;
+		} else if(inputIsValid) {		// if input valid, call editAccount from DataManager
+			// DataManager will have to check which inputs are null
+			// Could also use current value for fields that are not updated
+			// Ex. Member only wants to update lastName, autofill other fields with current values
+			accountUpdated = dataManager.editAccount(username, password, firstName, lastName);
+		}
+	
+		return accountUpdated;	
 	}
 }
