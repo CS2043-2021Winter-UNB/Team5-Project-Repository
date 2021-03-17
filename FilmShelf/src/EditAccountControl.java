a2a08cbe14dc912af80ba07c6d287c673f1dedb3
salamiman <josulaiman@hotmail.com>
2021-03-17 18:53:52 -0300

    Still working on EditAccountControl. - Modifying validation for editing account - Refactoring previous progress

--- FilmShelf/src/EditAccountControl.java
+++ FilmShelf/src/EditAccountControl.java
@@ -32,121 +32,123 @@ public class EditAccountControl {
 
 	// may need two validation methods with different signatures
 	// one to check member details, one to check admin details
-	public boolean validateFormInput(String username, String password, String firstName, String lastName) {
-		// input is invalid if any null inputs are provided
-		if((username == null) || (password == null) || (firstName == null) || (lastName == null)) {
+	public boolean validateFormInput(MemberAccountObject member, String username, String password, String firstName, String lastName, String description) {
+		String uName = "";
+		String pWord = "";
+		String fName = "";
+		String lName = "";
+		String desc = "";
+		
+		// input is invalid if no new details are provided
+		if(((username == null) || username.strip().isEmpty() || username.equals(member.getUsername())) &&
+			((password == null) || password.strip().isEmpty()) &&
+			((firstName == null) || firstName.strip().isEmpty() || firstName.equals(member.getFirstName())) &&
+			((lastName == null) || lastName.strip().isEmpty() || lastName.equals(member.getLastName())) &&
+			((description == null) || (description.equals(member.getDescription())))) {
 			return false;
 		}
 		
-		// remove leading and trailing whitespace and store in new variable
-		String uName = username.strip();
-		String pWord = password.strip();
-		String fName = firstName.strip();
-		String lName = lastName.strip();
+		// remove leading and trailing whitespace and store in new variable for non-null parameters
+		if(username != null) {
+			uName = username.strip();
+		}
+		if(password != null) {
+			pWord = password.strip();
+		}
+		if(firstName != null) {
+			fName = firstName.strip();
+		}
+		if(lastName != null) {
+			lName = lastName.strip();
+		}
+		if(description != null) {
+			desc = description.strip();
+		}
+		
 	
 		// check validity of each parameter
 		// check username
 			// minimum 6 characters
 			// maximum 25 characters
 			// no special symbols
-		specialCharacterMatcher = specialCharacterPattern.matcher(uName);
-	
-		if(specialCharacterMatcher.find() || (uName.length() > 25) || (uName.length() < 6)) {
-			return false;
+		if(!uName.strip().isEmpty() && !uName.equals(member.getUsername())) {
+			specialCharacterMatcher = specialCharacterPattern.matcher(uName);
+			
+			if(specialCharacterMatcher.find() || (uName.length() > 25) || (uName.length() < 6)) {
+				return false;
+			}
 		}
+		
 	
 		// check password
 			// minimum 8 characters
 			// contain uppercase letter, lowercase letter, number, special character
-		uppercaseMatcher = uppercasePattern.matcher(pWord);
-		lowercaseMatcher = lowercasePattern.matcher(pWord);
-		numberMatcher = numberPattern.matcher(pWord);
-		specialCharacterMatcher = specialCharacterPattern.matcher(pWord);
-	
-		if(!uppercaseMatcher.find() || !lowercaseMatcher.find() || !numberMatcher.find() || !specialCharacterMatcher.find()  || (pWord.length() < 8)) {
-			return false;
+		if(!pWord.strip().isEmpty()) {		// password needs to be handled separately
+			uppercaseMatcher = uppercasePattern.matcher(pWord);
+			lowercaseMatcher = lowercasePattern.matcher(pWord);
+			numberMatcher = numberPattern.matcher(pWord);
+			specialCharacterMatcher = specialCharacterPattern.matcher(pWord);
+		
+			if(!uppercaseMatcher.find() || !lowercaseMatcher.find() || !numberMatcher.find() || !specialCharacterMatcher.find()  || (pWord.length() < 8)) {
+				return false;
+			}
 		}
-	
+		
 		// check first name
 			// minimum 1 character
 			// maximum 25 characters
 			// no numbers or special characters
-		numberMatcher = numberPattern.matcher(fName);
-		specialCharacterMatcher = specialCharacterPattern.matcher(fName);
-	
-		if(numberMatcher.find() || specialCharacterMatcher.find() || (fName.length() > 25) || (fName.length() < 1)) {
-			return false;
+		if(!fName.strip().isEmpty() && !fName.equals(member.getFirstName())) {
+			numberMatcher = numberPattern.matcher(fName);
+			specialCharacterMatcher = specialCharacterPattern.matcher(fName);
+		
+			if(numberMatcher.find() || specialCharacterMatcher.find() || (fName.length() > 25) || (fName.length() < 1)) {
+				return false;
+			}
 		}
 	
 		// check last name
 			// minimum 1 character
 			// maximum 25 characters
 			// no numbers or special characters
-		numberMatcher = numberPattern.matcher(lName);
-		specialCharacterMatcher = specialCharacterPattern.matcher(lName);
-	
-		if(numberMatcher.find() || specialCharacterMatcher.find() || (lName.length() > 25) || (lName.length() < 1)) {
+		if(!lName.strip().isEmpty() && !lName.equals(member.getLastName())) {
+			numberMatcher = numberPattern.matcher(lName);
+			specialCharacterMatcher = specialCharacterPattern.matcher(lName);
+		
+			if(numberMatcher.find() || specialCharacterMatcher.find() || (lName.length() > 25) || (lName.length() < 1)) {
+				return false;
+			}
+		}
+		
+		// check description
+			// maximum 280 characters
+			// minimum 0 characters
+		if(desc.equals(member.getDescription()) || (desc.length() > 280)) {
 			return false;
 		}
 		
 		return true;	// if this line is reached, input is valid
 	}
 
-	// Jo: 	how to determine if Member or Admin wants to edit account?
-	//		can we assume only one logged in user stored?
-	// 		ie. either currentAdmin or currentMember will be uninitialized
-	//		what details can users update?
-	//			Member: username, password, firstName, lastName
-	//			Admin: username, password
-	//		if username can be changed, will have to check database to ensure new username doesn't already exist
-	//		could be easier to assume only Members change details via app, Admin have details changed directly in database
-	public boolean updateAccount(String username, String password, String firstName, String lastName) {
+	// Jo:	how to tell UI why edit failed?
+	//		TODO: if username can be changed, will have to check database to ensure new username doesn't already exist
+	// 		TODO: will need special case for changing password, as encrypted passwords can't be decrypted easily
+	//		UI could check if user is logged in before displaying edit account form and even pass member to Control
+	public boolean updateAccount(String username, String password, String firstName, String lastName, String description) {
 		boolean currentUserIsMember = false;
 		boolean inputIsValid = false;
 		boolean accountUpdated = false;
 		
 		// get current user logged in
 		MemberAccountObject member = loginControl.getCurrentMember();
-		AdminAccountObject admin = loginControl.getCurrentAdmin();
 		
-		// check if current user is member or admin
-		if((admin == null) && (member == null)) {
+		// if member not logged in, return false
+		if(member == null) {
 			return false;
-		} else if(admin == null) {
-			currentUserIsMember = true;
-		} else if(member == null) {
-			currentUserIsMember = false;
-		}
-		
-		// only continue with edit account if at least one new input is provided
-		if(((username == null) || username.strip().isEmpty() || username.equals(member.getUsername())) &&
-			((password == null) || password.strip().isEmpty() || password.equals(member.getPassword())) &&
-			((firstName == null) || firstName.strip().isEmpty() || firstName.equals(member.getFirstName())) &&
-			((lastName == null) || lastName.strip().isEmpty() || lastName.equals(member.getLastName()))) {
-			return false;
-		}
-		
-		// check which details the member wants to change and which stay the same
-		// assume values not provided by user indicate fields that stay the same
-		if((username == null) || username.strip().isEmpty()) {
-			username = member.getUsername();
-		}
-		if((password == null) || password.strip().isEmpty()) {
-			password = member.getPassword();
-		}
-		if((firstName == null) || firstName.strip().isEmpty()) {
-			firstName = member.getFirstName();
-		}
-		if((lastName == null) || lastName.strip().isEmpty()) {
-			lastName = member.getLastName();
 		}
 		
 		// check validity of form input
-		if(currentUserIsMember) {
-			inputIsValid = validateFormInput(username, password, firstName, lastName);
-		} else if(!currentUserIsMember) {
-			inputIsValid = validateFormInput(username, password);
-		}
+		inputIsValid = validateFormInput(member, username, password, firstName, lastName, description);
 		
 		// if input invalid, return false
 		if(!inputIsValid) {
@@ -155,7 +157,31 @@ public class EditAccountControl {
 			// DataManager will have to check which inputs are null
 			// Could also use current value for fields that are not updated
 			// Ex. Member only wants to update lastName, autofill other fields with current values
-			accountUpdated = dataManager.editAccount(username, password, firstName, lastName);
+			
+			// default values to pass to DataManager in case of null/empty inputs
+			String uName = member.getUsername();
+			String pWord = null;
+			String fName = member.getFirstName();
+			String lName = member.getLastName();
+			String desc = member.getDescription();
+			
+			if(username != null) {
+				uName = username.strip();
+			}
+			if(password != null) {
+				pWord = password.strip();
+			}
+			if(firstName != null) {
+				fName = firstName.strip();
+			}
+			if(lastName != null) {
+				lName = lastName.strip();
+			}
+			if(description != null) {
+				desc = description.strip();
+			}
+			
+			accountUpdated = dataManager.editAccount(uName, pWord, fName, lName, desc);
 		}
 	
 		return accountUpdated;	
