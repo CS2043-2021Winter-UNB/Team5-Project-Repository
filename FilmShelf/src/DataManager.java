58bf5a74cad30e919378ac10dc87e06b577df9e1
Courtney Perreault <courtneyperreault@Courtneys-MacBook-Air.local>
2021-03-20 15:14:31 -0300

    DataManager

--- FilmShelf/src/DataManager.java
+++ FilmShelf/src/DataManager.java
@@ -1,39 +1,17 @@
 
-import java.sql.*;
 import java.util.ArrayList;
 
 public class DataManager {
 
-<<<<<<< HEAD
 	//private AdminAccountObject adminAccount;
 	//private MovieObject movie;
 	//private ReviewObject review;
 	//private MemberAccountObject member;
-=======
-	public AdminAccountObject adminAccount;
-	public MovieObject movie;
-	public ReviewObject review;
-	public Connection connection = null;
 
-	public DataManager() {
-		 try {
-	         Class.forName("com.mysql.jdbc.Driver").newInstance();
-	     } catch (Exception e) {
-	      System.err.println(e.toString());
-	     }
-		String url = "jdbc:mysql://cs2043.cs.unb.ca:3306/cs2043team5";
-		try {
-		connection = DriverManager.getConnection(url, "cs2043team5", "E8mP1JDK");
-		} catch (SQLException e) {
-		System.err.println("Database connection error: " + e);
-		}
-	}
-	
-	
->>>>>>> 1f17b7175fc8a9f52405f1e67f05e17ae12fc47f
-
-	
 	public MemberAccountObject getMember(String username, String password) {
+
+		//Create MemberAccountObject 
+		MemberAccountObject member = new MemberAccountObject();
 
 		//create statement 
 		Statment stmt = conn.createStatement();
@@ -43,25 +21,71 @@ public class DataManager {
 						  "' and password = sha1(" + password ");";
 
 		//ResultSet 
-		ResultSet rs = stmt.executeQuery(sqlQuery);
+			ResultSet rs = stmt.executeQuery(sqlQuery);
 
-		//Create MemberAccountObject 
-		MemberAccountObject member = new MemberAccountObject();
-		member.username = rs.getString(1);
-		memeber.password = rs.getString(2);
-		member.simething = rs.getString(3);
-		memeber. something = rs.getString(4);
-		member. somethingelse = rs.getString(5);
+			//assigning values to memberAccountObject	
+			member.username = rs.getString(1);
+			memeber.password = rs.getString(2);
+			member.simething = rs.getString(3);
+			memeber. something = rs.getString(4);
+			member. somethingelse = rs.getString(5);
+		}
 
 		return member;
 		// end-user-code
 	}
 
-	public AdminAccountObject getAdmin(String username, String password) {
+	public AdminAccountObject getAdmin(String username, String password){
 		// begin-user-code
 		// TODO Auto-generated method stub
-		return null;
+		//create statement 
+		Statment stmt = conn.createStatement();
+
+		//Create MemberAccountObject 
+		AdminAccountObject member = new MemberAccountObject();
+
+		//SQL query String 
+		String sqlQuery = "select * from MemberAccount where username = '" + username +
+						  "' and password = sha1(" + password ");";
+
+		//ResultSet 
+		ResultSet rs = stmt.executeQuery(sqlQuery);
+
+		//Assigning values to adminAccountObject
+		admin.username = rs.getString(1);
+		admin.password = rs.getString(2);
+
+		//return adminAccountObject
+		return admin;
 		// end-user-code
+	}
+
+	public boolean addMemberAccount(String username, String password, String email, String firstName,
+								 String lastName) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+		//create statement 
+		Statment stmt = conn.createStatement();
+
+		//SQL query String 
+		String sqlQuery = "insert into MemberAccount values('" + username + "', '" + 
+
+		//ResultSet 
+		ResultSet rs = stmt.executeQuery(sqlQuery);
+
+		//Create MemberAccountObject 
+		MemberAccountObject member = new MemberAccountObject();
+	
+		return 
+		// end-user-code
+	}
+
+	public int editMemberAccount(String username, String[] values){
+		//String query 
+		if(values[1] != null){
+			stmt.executeQuery()
+		}
+		
 	}
 
 	public int getMovieRatingByMember(String username, int movieID) {
@@ -83,20 +107,6 @@ public class DataManager {
 		// begin-user-code
 		// TODO Auto-generated method stub
 		return null;
-		// end-user-code
-	}
-
-	public void addMemberAccount(String username, String password, String email) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
-	}
-
-	public void addMovieRequest(Object movieRequest) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
 		// end-user-code
 	}
 
