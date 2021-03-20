402b9c6caa2ba8b60e5a9ad112a1a8336b3f07a9
salamiman <josulaiman@hotmail.com>
2021-03-20 13:19:35 -0300

    Further developed SearchAccountControl. - Added class constructor - Completed processSearchAccount() - Completed validateSearchInput()

--- FilmShelf/src/SearchAccountControl.java
+++ FilmShelf/src/SearchAccountControl.java
@@ -1,19 +1,57 @@
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+
+/******************************************************************************************************************************
+ * SearchAccountControl
+ * @author Jo
+ * Description:	Handles validation of search input to SearchAccountUI class and initiates searching the MemberAccount table.
+ ******************************************************************************************************************************/
 public class SearchAccountControl {
 
 	private DataManager dataManager;
-
-	public MemberAccountObject processSearchAccount(String username) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+	private MemberAccountObject member;
+	//private ArrayList<MemberAccountObject> members;
+	private Pattern specialCharacterPattern;
+	private Matcher specialCharacterMatcher;
+	
+	public SearchAccountControl(DataManager dataManager) {
+		this.dataManager = dataManager;
+		this.member = null;
+		//members = new ArrayList<MemberAccountObject>();
+		this.specialCharacterPattern = Pattern.compile("[^A-Za-z0-9]");		// matches special characters
 	}
 
-	public boolean IsSearchInputValid(String username) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return false;
-		// end-user-code
+	// Jo: could return ArrayList if we want to allow for generalized searches (not just exact matches)
+	public MemberAccountObject processSearchAccount(String username) {
+		// check validity of input
+		if((username != null) && validateSearchInput(username.strip())) {
+			// search for member if input valid
+			member = dataManager.searchMemberByUsername(username.strip());		// Jo: went with this method name, could also go with getMember(username) since we have getMember(username, password)
+		} else {
+			// input not valid, set member to null
+			member = null;
+		}
+		
+		// return search result
+		return member;
+	}
+
+	// invalid input includes null and strings containing special characters
+	public boolean validateSearchInput(String username) {
+		// null input is invalid
+		if(username == null) {
+			return false;
+		}
+		
+		// no special characters allowed
+		specialCharacterMatcher = specialCharacterPattern.matcher(username.strip());
+		
+		if(specialCharacterMatcher.find()) {
+			return false;
+		}
+		
+		return true;	// if this line is reached, input is valid
 	}
 }
