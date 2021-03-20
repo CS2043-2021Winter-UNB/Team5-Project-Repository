ae9dbabb7f4c324abcfa0f6daac67c1563350a0b
salamiman <josulaiman@hotmail.com>
2021-03-20 00:32:52 -0300

    Removed username handling from EditAccountControl. - Added a test folder to the project for JUnit test cases. - Wrote unit tests to check valid and invalid inputs in CreateMemberAccountControl and EditAccountControl.

--- FilmShelf/src/EditAccountControl.java
+++ FilmShelf/src/EditAccountControl.java
@@ -30,16 +30,14 @@ public class EditAccountControl {
 		this.specialCharacterPattern = Pattern.compile("[^A-Za-z0-9]");		// matches special characters
 	}
 
-	public boolean validateFormInput(MemberAccountObject member, String username, String password, String firstName, String lastName, String description) {
-		String uName = "";
+	public boolean validateFormInput(MemberAccountObject member, String password, String firstName, String lastName, String description) {
 		String pWord = "";
 		String fName = "";
 		String lName = "";
 		String desc = "";
 		
 		// input is invalid if no new details are provided
-		if(((username == null) || username.strip().isEmpty() || username.equals(member.getUsername())) &&
-			((password == null) || password.strip().isEmpty()) &&
+		if(((password == null) || password.strip().isEmpty()) &&
 			((firstName == null) || firstName.strip().isEmpty() || firstName.equals(member.getFirstName())) &&
 			((lastName == null) || lastName.strip().isEmpty() || lastName.equals(member.getLastName())) &&
 			((description == null) || (description.equals(member.getDescription())))) {
@@ -47,9 +45,6 @@ public class EditAccountControl {
 		}
 		
 		// remove leading and trailing whitespace and store in new variable for non-null parameters
-		if(username != null) {
-			uName = username.strip();
-		}
 		if(password != null) {
 			pWord = password.strip();
 		}
@@ -63,25 +58,11 @@ public class EditAccountControl {
 			desc = description.strip();
 		}
 		
-	
 		// check validity of each parameter
-		// check username
-			// minimum 6 characters
-			// maximum 25 characters
-			// no special symbols
-		if(!uName.strip().isEmpty() && !uName.equals(member.getUsername())) {
-			specialCharacterMatcher = specialCharacterPattern.matcher(uName);
-			
-			if(specialCharacterMatcher.find() || (uName.length() > 25) || (uName.length() < 6)) {
-				return false;
-			}
-		}
-		
-	
 		// check password
 			// minimum 8 characters
 			// contain uppercase letter, lowercase letter, number, special character
-		if(!pWord.strip().isEmpty()) {		// password needs to be handled separately
+		if(!pWord.strip().isEmpty()) {
 			uppercaseMatcher = uppercasePattern.matcher(pWord);
 			lowercaseMatcher = lowercasePattern.matcher(pWord);
 			numberMatcher = numberPattern.matcher(pWord);
@@ -121,7 +102,7 @@ public class EditAccountControl {
 		// check description
 			// maximum 280 characters
 			// minimum 0 characters
-		if(desc.equals(member.getDescription()) || (desc.length() > 280)) {
+		if(desc.length() > 280) {
 			return false;
 		}
 		
@@ -130,7 +111,7 @@ public class EditAccountControl {
 
 	// Jo:	how to tell UI why edit failed?
 	//		UI could check if user is logged in before displaying edit account form and even pass member to Control
-	public boolean updateAccount(String username, String password, String firstName, String lastName, String description) {
+	public boolean updateAccount(String password, String firstName, String lastName, String description) {
 		boolean currentUserIsMember = false;
 		boolean inputIsValid = false;
 		boolean accountUpdated = false;
@@ -144,27 +125,19 @@ public class EditAccountControl {
 		}
 		
 		// check validity of form input
-		inputIsValid = validateFormInput(member, username, password, firstName, lastName, description);
+		inputIsValid = validateFormInput(member, password, firstName, lastName, description);
 		
 		// if input invalid, return false
 		if(!inputIsValid) {
 			accountUpdated = false;
 		} else if(inputIsValid) {		// if input valid, call editAccount from DataManager
 			// default values to pass to DataManager in case of null/empty inputs
-			String uName = member.getUsername();
 			String pWord = null;
 			String fName = member.getFirstName();
 			String lName = member.getLastName();
 			String desc = member.getDescription();
 			
 			// prepare to pass inputs that aren't null/empty
-			if(username != null && !username.strip().isEmpty()) {
-				uName = username.strip();
-				
-				if(dataManager.doesUsernameAlreadyExist(uName)) {	// check MemberAccount table for matching username
-					return false;
-				}
-			}
 			if(password != null && !password.strip().isEmpty()) {
 				pWord = password.strip();
 			}
@@ -178,7 +151,7 @@ public class EditAccountControl {
 				desc = description.strip();
 			}
 			
-			accountUpdated = dataManager.editAccount(uName, pWord, fName, lName, desc);		// DataManager will have to check for null password
+			accountUpdated = dataManager.editAccount(pWord, fName, lName, desc);		// DataManager will have to check for null password
 		}
 	
 		return accountUpdated;	
