116e177c567b387928a1b60ced79e236f56a2b32
Rachel Savidge <Rachel Savidge@Rachel-PC>
2021-03-31 10:30:25 -0300

    RemoveMovieControl added

--- FilmShelf/src/RemoveMovieControl.java
+++ FilmShelf/src/RemoveMovieControl.java
@@ -1,13 +1,33 @@
-
+/******************************************************************************************************************************
+ * RemoveMovieControl
+ * @author Rachel
+ * Description:	Handles validation of input to removeMovie class, including user (admin), and initiates movie removal in database.
+ ******************************************************************************************************************************/
 public class RemoveMovieControl {
 
 	private DataManager dataManager;
 	private LoginControl loginControl;
+	private MovieObject movie;
+	
 
-	public void processRemoveMovie() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	public RemoveMovieControl(DataManager dm, LoginControl loginControl, MovieObject movie) {
+		this.dataManager = dm;
+		this.loginControl = loginControl;
+		this.movie = movie;
+	}
+	
+	public boolean processRemoveMovie() {
+		
+		//check method name
+		AdminObject admin = loginControl.getCurrentAdmin();
+		
+		// only permit addition of movie if an admin is logged in
+		if (admin == null) {
+			return false;
+		}
+		
+		//Call data manager to remove movie
+		return dataManager.removeMovie(movie.getMovieId());
+				
 	}
 }
