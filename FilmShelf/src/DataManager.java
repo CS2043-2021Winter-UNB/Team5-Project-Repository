fca631fd4586e9b90e55a3ddf1aeeb4aa95c6b64
salamiman <josulaiman@hotmail.com>
2021-04-05 15:18:08 -0300

    Reviewed updates to DataManager and made changes as needed.

--- FilmShelf/src/DataManager.java
+++ FilmShelf/src/DataManager.java
@@ -23,12 +23,10 @@ public class DataManager {
 
 		try {
 			connection = DriverManager.getConnection(url, "cs2043team5", "E8mP1JDK");
-		} 
-		catch(SQLException e) {
+		} catch(SQLException e) {
 			System.err.println("Database connection error: " + e);
 		}
 	}
-
 
 	//Courtney and Jo
 	public boolean addMember(String username, String password, String firstName, String lastName, String description) {
@@ -43,8 +41,12 @@ public class DataManager {
 
 			//ResultSet
 			int rowsUpdated = stmt.executeUpdate(sqlQuery);
-		}
-		catch(SQLException e) {
+			
+			// return false if no member was added
+			if(rowsUpdated == 0) {
+				return false;
+			}
+		} catch(SQLException e) {
 			System.out.println("Add member error: " + e.getMessage());
 			return false;
 		}
@@ -52,40 +54,35 @@ public class DataManager {
 		return true;
 	}
 
-
 	//Courtney and Jo
 	//Make sure null fields aren't null in the database!!!!!
 	public boolean editMember(String username, String password, String firstName, String lastName, String description){
 		//String query 
-		String sqlQuery = "update MemberAccount set firstName = '" + firstName + "', lastName = '" + lastName 
-							+ "', description = '" + description + "'";;
+		String sqlQuery = "update MemberAccount set firstName = '" + firstName + "', lastName = '" + lastName + "', description = '" + description + "'";
+		
+		//If new value is passed in update password
+		if(password != null){
+			sqlQuery = sqlQuery + ", password = sha1('" + password + "')";
+		}
+		sqlQuery = sqlQuery + " where username = '" + username + "';";
 
 		try {
 			//Create Statement 
 			Statement stmt = connection.createStatement();
 
-			//If new value is passed in update password
-			if(password != null){
-
-				sqlQuery = sqlQuery + ", password = sha1('" + password + "')";
-
-			}
-
-
-			sqlQuery = sqlQuery + " where username = '" + username + "';";
-
 			int rowsUpdated = stmt.executeUpdate(sqlQuery);
-
-		}
-		catch(SQLException e) {
+			
+			// return false if no member was edited
+			if(rowsUpdated == 0) {
+				return false;
+			}
+		} catch(SQLException e) {
 			System.out.println("Edit member error: " + e.getMessage());
 			return false;
 		}
 
 		return true;
 	}
-
-
 
 	//Courtney and Jo 
 	public boolean removeMember(String username) {
@@ -97,16 +94,19 @@ public class DataManager {
 			Statement stmt = connection.createStatement();
 
 			//Execute query
-			stmt.executeUpdate(sqlQuery);
-		}
-		catch(SQLException e) {
+			int rowsUpdated = stmt.executeUpdate(sqlQuery);
+			
+			// return false if no member was removed
+			if(rowsUpdated == 0) {
+				return false;
+			}
+		} catch(SQLException e) {
 			System.out.println("Remove member error: " + e.getMessage());
 			return false;
 		}
 
 		return true;
 	}
-
 
 	//Courtney and Jo (make changes based on top movies)
 	public MemberObject getMember(String username, String password) {
@@ -118,7 +118,6 @@ public class DataManager {
 		String description = "";
 		ArrayList<MovieObject> topMovies;
 		
-
 		//SQL query String 
 		String sqlQuery = "select * from MemberAccount where username = '" + username + "' and password = sha1('" + password + "');";
 
@@ -128,8 +127,11 @@ public class DataManager {
 
 			//ResultSet 
 			ResultSet rs = stmt.executeQuery(sqlQuery);
-
-		
+			
+			// return null if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return null;
+			}
 
 			//Move Cursor to First Row
 			rs.next();
@@ -139,24 +141,15 @@ public class DataManager {
 			firstName = rs.getString(3);
 			lastName = rs.getString(4);
 			description = rs.getString(5);
-			topMovies = this.getTopMovies(username);
-
-	
-		}
-		catch(SQLException e) {
-			System.out.println();
+			topMovies = getTopMovies(username);
+		} catch(SQLException e) {
 			System.out.println("Member login error: " + e.getMessage());
 			return null;
 		}
 		
-		
 		// Create MemberObject to return
 		return new MemberObject(uName, firstName, lastName, description, topMovies);
-
-		
 	}
-
-
 
 	//getMember for View Member Case
 	public MemberObject getMember(String username){
@@ -172,12 +165,16 @@ public class DataManager {
 		String sqlQuery = "select *  from MemberAccount where username = '" + username + "';";
 
 		try{
-
 			//Create Statement 
 			Statement stmt = connection.createStatement();
 
 			//Initialize Result Set
 			ResultSet rs = stmt.executeQuery(sqlQuery);
+			
+			// return null if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return null;
+			}
 
 			//Move Cursor to First Row
 			rs.next();
@@ -187,20 +184,15 @@ public class DataManager {
 			firstName = rs.getString(3);
 			lastName = rs.getString(4);
 			description = rs.getString(5);
-			topMovies = this.getTopMovies(username);
-
-		}
-		catch(SQLException e){
+			topMovies = getTopMovies(username);
+		} catch(SQLException e) {
 			System.out.println("View Member Error: " + e.getMessage());
 			return null;
 		}
 
 		// Create MemberObject to return
 		return new MemberObject(uName, firstName, lastName, description, topMovies);
-
-
 	}
-
 
 	//Courtney and Jo
 	public AdminObject getAdmin(String username, String password){
@@ -215,21 +207,22 @@ public class DataManager {
 			Statement stmt = connection.createStatement();
 			//ResultSet 
 			ResultSet rs = stmt.executeQuery(sqlQuery);
+			
+			// return null if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return null;
+			}
 
 			//Assigning values to adminObject
 			rs.next();		// need to call to point cursor to first record
 			uName = rs.getString(1);
-		}
-		catch(SQLException e) {
+		} catch(SQLException e) {
 			System.out.println("Admin login error: " + e.getMessage());
 		}
 
 		//return adminObject
 		return new AdminObject(uName);
 	}
-
-	
-
 
 	//Jessie-Anne 
 	public boolean addMovie(String title, int releaseYear, String genre, int length){
@@ -243,6 +236,11 @@ public class DataManager {
 
 			//ResultSet
 			int rowsUpdated = stmt.executeUpdate(sqlQuery);
+			
+			// return false if no movie was added
+			if(rowsUpdated == 0) {
+				return false;
+			}
 		}
 		catch(SQLException e) {
 			System.out.println("Add movie error: " + e.getMessage());
@@ -251,7 +249,6 @@ public class DataManager {
 
 		return true;
 	}
-
 
 	//Jessie-Anne
 	public boolean removeMovie(int movieID) {
@@ -263,9 +260,13 @@ public class DataManager {
 			Statement stmt = connection.createStatement();
 
 			//Execute query
-			stmt.executeUpdate(sqlQuery);
-		}
-		catch(SQLException e) {
+			int rowsUpdated = stmt.executeUpdate(sqlQuery);
+			
+			// return false if no member was removed
+			if(rowsUpdated == 0) {
+				return false;
+			}
+		} catch(SQLException e) {
 			System.out.println("Remove movie error: " + e.getMessage());
 			return false;
 		}
@@ -273,10 +274,8 @@ public class DataManager {
 		return true;
 	}
 
-
-
 	//Courtney 
-	public ArrayList<MovieObject> getMoviesbyKeywords(ArrayList<String> title, int  minReleaseYear, int maxReleaseYear, String genre, int minLength, int maxLength) {
+	public ArrayList<MovieObject> getMoviesbyKeywords(String title, int  minReleaseYear, int maxReleaseYear, String genre, int minLength, int maxLength) {
 
 		//Creating MovieArrayList to return to user
 		ArrayList<MovieObject> movieList = new ArrayList<MovieObject>();
@@ -288,10 +287,8 @@ public class DataManager {
 		String sqlQuery = "select movieID from Movie where";
 
 		//Add Desired title to Query (does not need to be exact)
-		for (int i=0; i<title.size(); i++) {
-			if (i < title.size() - 1)
-				sqlQuery = sqlQuery + " title like '%" + title.get(i) + "%' or ";
-			else sqlQuery = sqlQuery + " title like '%" + title.get(i) + "%'";
+		if(title != null) {
+			sqlQuery = sqlQuery + "title like '%" + title + "%'";
 		}
 
 		//Add Desired Genre to Query 
@@ -300,68 +297,58 @@ public class DataManager {
 		}
 		
 		//Add Desired Release Year to Query
-		if(minReleaseYear != -1 && maxReleaseYear != -1){
-			if(minReleaseYear == maxReleaseYear){
-				sqlQuery = sqlQuery + " and releaseYear = " + minReleaseYear ; 
- 			}
-			else{
-				sqlQuery = sqlQuery + " and releaseYear between " + minReleaseYear + " and " + maxReleaseYear ;
+		if(minReleaseYear != -1 && maxReleaseYear != -1) {
+			if(minReleaseYear == maxReleaseYear) {
+				sqlQuery = sqlQuery + " and releaseYear = " + minReleaseYear; 
+ 			} else {
+				sqlQuery = sqlQuery + " and releaseYear between " + minReleaseYear + " and " + maxReleaseYear;
 			}
-		
 		} 
 		
-
-
 		//Add Desired Length to Query  
-		if(minLength != -1 && maxLength != -1){
-			if(minLength == maxLength){
-				sqlQuery = sqlQuery + " and releaseYear = " + minLength + ""; 
+		if(minLength != -1 && maxLength != -1) {
+			if(minLength == maxLength) {
+				sqlQuery = sqlQuery + " and length = " + minLength; 
  			}
 			else{
-				sqlQuery = sqlQuery + " and releasYear between " + minLength + " and " + maxLength ;
+				sqlQuery = sqlQuery + " and length between " + minLength + " and " + maxLength;
 			}
-		
 		} 
 		
-
 		//Add semi-colon to end statement (must be done at end since end may vary)
 		sqlQuery = sqlQuery + ";";
-
 
 		try{
 			//Create Statement
 			Statement stmt = connection.createStatement();
 
-			
 			//Result Set
 			ResultSet rs = stmt.executeQuery(sqlQuery);
+			
+			// return empty ArrayList if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return new ArrayList<MovieObject>();
+			}
 
 			//Create Movie Objects and Add to Movie List
-			while (rs.next()) {
-
+			while(rs.next()) {
 				movieIdList.add(rs.getInt(1));
-
 			}
 
 			for(int index = 0; index < movieIdList.size(); index++){
-				movieList.add(this.getMovie(movieIdList.get(index)));
+				movieList.add(getMovie(movieIdList.get(index)));
 			}
-
-		}
-
-		catch(SQLException e){
-			System.out.println(e.getMessage());
+		} catch(SQLException e) {
+			System.out.println("Search Movie error: " + e.getMessage());
 			return null;		
 		}
 
 		return movieList;
-
 	}
 
 	//Courtney 
 	//For View Movie (and getTopMovies)
-	public MovieObject getMovie(int movieID){
-
+	public MovieObject getMovie(int movieID) {
 		//Initialize Parameters for Constructor 
 		String title;
 		int releaseYear;
@@ -374,30 +361,31 @@ public class DataManager {
 		String sqlQuery = "select * from Movie where movieID = " + movieID + ";";
 
 		try{
-
 			//Create Statement 
 			Statement stmt = connection.createStatement();
 
 			//Initialize ResultSet
 			ResultSet rs = stmt. executeQuery(sqlQuery);
+			
+			// return null if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return null;
+			}
 
 			rs.next();
+			
 			title = rs.getString(1);
 			releaseYear = rs.getInt(2);
 			genre = rs.getString(3);
 			length = rs.getInt(4);
 			movieId = rs.getInt(5);
-			averageRating = (this.getAverageRating(movieId));
-
-			
-		}
-		catch(SQLException e){
+			averageRating = (getAverageRating(movieId));
+		} catch(SQLException e) {
 			System.out.println("Get Movie Error: " + e.getMessage());
 			return null;
 		}
 
 		return new MovieObject(title, releaseYear, genre, length, averageRating, movieId);
-
 	}
 
 
@@ -439,12 +427,10 @@ public class DataManager {
 
 	//Courtney 
 	public RatingObject getMovieRatingByMember(String username, int movieID) {
-
 		//Initilaize Parameters for Constructor
 		int ratingScore;
-		String uName = " ";
+		String uName = "";
 		int movieId;
-
 
 		//SQL String query
 		String sqlQuery = "select * from Rating where username = '" + username + "' and movieID = " + movieID + ";";
@@ -456,26 +442,22 @@ public class DataManager {
 			//ResultSet 
 			ResultSet rs = stmt.executeQuery(sqlQuery);
 			
+			// return null if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return null;
+			}
+			
 			//Movie Cursor to first Row
 			rs.next();
 
 			ratingScore = rs.getInt(1);
 			uName = rs.getString(2);
 			movieId = rs.getInt(3);
-
-
-
-		}
-
-		catch(SQLException e){
+		} catch(SQLException e) {
 			return null;
 		}
 
-
 		return new RatingObject(ratingScore, uName, movieId);
-
-
-		// end-user-code
 	}
 
 
@@ -502,11 +484,10 @@ public class DataManager {
 
 	//Courtney 
 	public ArrayList<ReviewObject> getMovieReviews(int movieID) {
-
 		//Initialize Parameters for Constructor 
-		String uName = " ";
+		String uName = "";
 		int movID;
-		String revText = " ";
+		String revText = "";
 		int revID;
 
 		// Create ArrayList of ReviewObjects
@@ -516,63 +497,62 @@ public class DataManager {
 		String sqlQuery = "select * from Reviews where movieID = " + movieID + ";";
 
 		try{
-
 			//Create Statement 
 			Statement stmt = connection.createStatement();
 
 			//ResultSet 
 			ResultSet rs = stmt.executeQuery(sqlQuery);
 
+			// return empty ArrayList if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return new ArrayList<ReviewObject>();
+			}
+			
 			//Add ReviewObjects to reviewList
 			while(rs.next()){
-
 				uName = rs.getString(1);
 				movID = rs.getInt(2);
 				revText = rs.getString(3);
 				revID = rs.getInt(4);
-
-				ReviewObject review = new ReviewObject(movID, uName, revText, revID);
-				reviewList.add(review);
+				reviewList.add(new ReviewObject(movID, uName, revText, revID));
 			}
-		}
-		catch(SQLException e){
-			System.out.println(e.getMessage());
+		} catch(SQLException e) {
+			System.out.println("Get Review error: " + e.getMessage());
 			return null;
 		}
 
 		return reviewList;
-
 	}
 
 
 	//Courtney
 	//Helper method for getMember method. 
-	private ArrayList<MovieObject> getTopMovies(String username){
+	private ArrayList<MovieObject> getTopMovies(String username) {
 
-		
 		//List of MovieIDs 
 		ArrayList<Integer> movieIdList = new ArrayList<Integer>();
 
 		//SQL String Query
-		String sqlQuery = "select movieID from Rating where username = '" + username +
-						  "' order by ratingScore desc;";
+		String sqlQuery = "select movieID from Rating where username = '" + username + "' order by ratingScore desc;";
 
 		try{
-
 			//Create Statement
 			Statement stmt = connection.createStatement();
 
 			//Initialize ResultSet
 			ResultSet rs = stmt.executeQuery(sqlQuery);
+			
+			// return empty ArrayList if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return new ArrayList<MovieObject>();
+			}
 
 			int numMovie = 0;
 			while(rs.next() && numMovie < NUM_TOP_MOVIES){
 				movieIdList.add(rs.getInt(1));
 				numMovie++;
 			}
-
-		}
-		catch(SQLException e){
+		} catch(SQLException e) {
 			System.out.println("Get Top Movies Error: " + e.getMessage());
 			return null;
 		}
@@ -580,22 +560,15 @@ public class DataManager {
 		//List of MovieObjects to return to calling method
 		ArrayList<MovieObject> movieList = new ArrayList<MovieObject>();
 
-
 		//Add Movie Objects to Movie List
-		for(int index = 0; index < NUM_TOP_MOVIES; index++){
-			movieList.add(this.getMovie(movieIdList.get(index)));
+		for(int index = 0; index < movieIdList.size(); index++) {
+			movieList.add(getMovie(movieIdList.get(index)));
 		}
 
-
 		return movieList;
-
 	}
 
-	
-
-
-	private double getAverageRating(int movieID){
-
+	private double getAverageRating(int movieID) {
 		//Initilaze return Varaiable
 		double averageRating = -1; 
 
@@ -603,26 +576,24 @@ public class DataManager {
 		String sqlQuery = "select avg(ratingScore) from Rating where movieID = " + movieID + ";";
 
 		try{
-
 			//Create Statement 
 			Statement stmt = connection.createStatement();
 
 			//Initilize ResultSet 
 			ResultSet rs = stmt.executeQuery(sqlQuery);
+			
+			// return 0 rating if ResultSet is empty
+			if(!rs.isBeforeFirst() && (rs.getRow() == 0)) {
+				return 0;
+			}
 
 			rs.next();
 			averageRating = rs.getDouble(1);
-
-
-		}
-
-		catch(SQLException e){
+		} catch(SQLException e) {
 			System.out.println("Get Average Rating Error: " + e.getMessage());
 		}
 
 		return averageRating;
-
-
 	}
 }
 
