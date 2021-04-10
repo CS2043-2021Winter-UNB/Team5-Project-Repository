522bd435c75ce3fa44eb7ad1a7dfcaf2343a48d1
Sharon MacLean <smaclea6@unb.ca>
2021-04-10 03:47:40 -0300

    Created LengthRestrictedDocument that extends PlainDocument and restricts the character limit for the text fields

---
+++ FilmShelf/src/LengthRestrictedDocument.java
@@ -0,0 +1,23 @@
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public final class LengthRestrictedDocument extends PlainDocument {
+  private static final long serialVersionUID = 1L;
+  private final int limit;
+
+  public LengthRestrictedDocument(int limit) {
+	super();
+    this.limit = limit;
+  }
+
+  public void insertString(int offset, String str, AttributeSet a) throws BadLocationException{
+    if (str == null)
+      return;
+
+    if ((getLength() + str.length()) <= limit) {
+      super.insertString(offset, str, a);
+    }
+  }
+}
